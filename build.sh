adfe609e1cc59758c6926c85d268f36a90185a72
Azmek <68204002+Azmekk@users.noreply.github.com>
2024-09-25 07:30:10 +0300

    Wrote a database migrator to move old db to new version

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_7z>"
+    exit 1
+fi
+
+SEVEN_ZIP="$1"
+APP_NAME="YT-DLP-Web-App-Backend" 
+OUTPUT_DIR="./bin/github-release"
+
+echo "Building backend..."
+cd ./src/backend/YT-DLP-Web-App-Backend
+
+dotnet restore
+
+dotnet publish -c Release -r win-x64 --self-contained true -o "$OUTPUT_DIR/win-x64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
+dotnet publish -c Release -r linux-x64 --self-contained true -o "$OUTPUT_DIR/linux-x64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
+dotnet publish -c Release -r win-arm64 --self-contained true -o "$OUTPUT_DIR/windows-arm64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
+dotnet publish -c Release -r linux-arm64 --self-contained true -o "$OUTPUT_DIR/linux-arm64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
+
+echo "Done!"
+
+echo "Building frontend..."
+cd ../../frontend
+pnpm install
+pnpm build
+
+cd ../backend/YT-DLP-Web-App-Backend
+
+for arch in win-x64 linux-x64 win-arm64 linux-arm64; do
+    if [ ! -d "$OUTPUT_DIR/$arch/Static" ]; then
+        mkdir -p "$OUTPUT_DIR/$arch/Static"
+    fi
+done
+
+echo "Copying files..."
+
+cp -r ../../frontend/build/* "$OUTPUT_DIR/win-x64/Static"
+cp -r ../../frontend/build/* "$OUTPUT_DIR/linux-x64/Static"
+cp -r ../../frontend/build/* "$OUTPUT_DIR/win-arm64/Static"
+cp -r ../../frontend/build/* "$OUTPUT_DIR/linux-arm64/Static"
+
+echo "Done!"
+
+echo "Compressing..."
+"$SEVEN_ZIP" a -tzip "$OUTPUT_DIR/linux-x64.zip" "$OUTPUT_DIR/linux-x64/*" -mx=9
+"$SEVEN_ZIP" a -tzip "$OUTPUT_DIR/linux-arm64.zip" "$OUTPUT_DIR/linux-arm64/*" -mx=9
+"$SEVEN_ZIP" a -tzip "$OUTPUT_DIR/win-x64.zip" "$OUTPUT_DIR/win-x64/*" -mx=9
+"$SEVEN_ZIP" a -tzip "$OUTPUT_DIR/win-arm64.zip" "$OUTPUT_DIR/win-arm64/*" -mx=9
+echo "Done!"
--- src/backend/DatabaseMigrator/buildScript.sh
+++ src/backend/DatabaseMigrator/buildScript.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <path_to_7z>"
@@ -6,11 +6,10 @@ if [ "$#" -ne 1 ]; then
 fi
 
 SEVEN_ZIP="$1"
-APP_NAME="YT-DLP-Web-App-Backend" 
+APP_NAME="DatabaseMigrator" 
 OUTPUT_DIR="./bin/github-release"
 
 echo "Building backend..."
-cd ./src/backend/YT-DLP-Web-App-Backend
 
 dotnet restore
 
@@ -18,28 +17,6 @@ dotnet publish -c Release -r win-x64 --self-contained true -o "$OUTPUT_DIR/win-x
 dotnet publish -c Release -r linux-x64 --self-contained true -o "$OUTPUT_DIR/linux-x64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
 dotnet publish -c Release -r win-arm64 --self-contained true -o "$OUTPUT_DIR/windows-arm64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
 dotnet publish -c Release -r linux-arm64 --self-contained true -o "$OUTPUT_DIR/linux-arm64" -p:PublishSingleFile=true "$APP_NAME.csproj" || exit 1
-
-echo "Done!"
-
-echo "Building frontend..."
-cd ../../frontend
-pnpm install
-pnpm build
-
-cd ../backend/YT-DLP-Web-App-Backend
-
-for arch in win-x64 linux-x64 win-arm64 linux-arm64; do
-    if [ ! -d "$OUTPUT_DIR/$arch/Static" ]; then
-        mkdir -p "$OUTPUT_DIR/$arch/Static"
-    fi
-done
-
-echo "Copying files..."
-
-cp -r ../../frontend/build/* "$OUTPUT_DIR/win-x64/Static"
-cp -r ../../frontend/build/* "$OUTPUT_DIR/linux-x64/Static"
-cp -r ../../frontend/build/* "$OUTPUT_DIR/win-arm64/Static"
-cp -r ../../frontend/build/* "$OUTPUT_DIR/linux-arm64/Static"
 
 echo "Done!"
 
